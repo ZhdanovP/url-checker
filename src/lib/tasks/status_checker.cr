6ebc650aec70a14af795b12e6becb5c11d7ab9e5
ZhdanovP <pavel.zhdanov@neuralegion.com>
2021-07-09 22:22:22 +0300

    Refactoring
    Extract workers to the modules

---
+++ src/lib/tasks/status_checker.cr
@@ -0,0 +1,21 @@
+require "http/client"
+
+module StatusChecker
+  private def self.get_status(url : String)
+    response = HTTP::Client.get url
+    {url, response.status_code}
+  rescue e : IO::Error | Socket::Addrinfo::Error
+    {url, e}
+  end
+
+  def self.run(url_stream, result_stream)
+    spawn do
+      loop do
+        url = url_stream.receive
+        result = get_status(url)
+
+        result_stream.send result
+      end
+    end
+  end
+end
--- src/url-checker.cr
+++ src/url-checker.cr
@@ -1,85 +1,26 @@
-require "http/client"
-require "yaml"
-require "tablo"
+require "./lib/tasks/printer"
+require "./lib/tasks/stats_logger"
+require "./lib/tasks/status_checker"
+require "./lib/tasks/url_generator"
 
-get_urls = ->{
-  file_lines = File.read("./urls.yml")
-  YAML.parse(file_lines)["urls"].as_a.map(&.as_s)
-}
-
-get_status = ->(url : String) {
-  begin
-    # puts "calling #{url}"
-    response = HTTP::Client.get url
-    {url, response.status_code}
-  rescue e : IO::Error | Socket::Addrinfo::Error
-    {url, e}
-    # ensure
-    # puts "called #{url}"
-  end
-}
-
+WORKERS = 2
 url_stream = Channel(String).new
 result_stream = Channel({String, Int32 | Exception}).new
+stats_stream = Channel(Array({String, Stats::Info})).new
 
-spawn do
-  get_urls.call.each { |url|
-    url_stream.send url
-  }
-end
+UrlGenerator.run("./urls.yml", url_stream)
 
-2.times {
-  spawn do
-    loop do
-      url = url_stream.receive
-      result = get_status.call(url)
-      result_stream.send result
-    end
-  end
+WORKERS.times {
+  StatusChecker.run(url_stream, result_stream)
 }
 
-stats = Hash(String, {success: Int32, failure: Int32}).new({success: 0, failure: 0})
-loop do
-  url, result = result_stream.receive
-  curent_value = stats[url]
+StatsLogger.run(result_stream, stats_stream)
 
-  case result
-  when Int32
-    if result < 400
-      stats[url] = {
-        success: curent_value["success"] + 1,
-        failure: curent_value["failure"],
-      }
-    else
-      stats[url] = {
-        success: curent_value["success"],
-        failure: curent_value["failure"] + 1,
-      }
-    end
-  when Exception
-    stats[url] = {
-      success: curent_value["success"],
-      failure: curent_value["failure"] + 1,
-    }
-  end
-
-  data = stats.map { |k, v|
-    [k, v["success"], v["failure"]]
-  }
-  table = Tablo::Table.new(data) do |t|
-    t.add_column("Url", width: 24) { |n| n[0] }
-    t.add_column("Success") { |n| n[1] }
-    t.add_column("Failure") { |n| n[2] }
-  end
-  puts table
-end
-
-# p get_urls.call # Array(string)
-#   .map(&get_status).join("\n")
+Printer.run(stats_stream)
 
 # url generator -> [url] -> worker_0 -> [{url, result}] -. print
 #                        \_ worker_1 _/
 
-# p get_status.call(get_urls.call.first)
+sleep
 
 puts "goodbye"
